b24ac6aac789a4c03f84e120ce630a40c32556a3
Andrey Shustariov <shustarev.andrey@gmail.com>
2014-04-19 16:38:57 +0300

    Removed logger from service factory

--- src/core/servicefactory/ServiceFactory.js
+++ src/core/servicefactory/ServiceFactory.js
@@ -1,26 +1,23 @@
 define(['module', 'src/core/loggerfactory/LoggerFactory', 'src/ngModule', 'lodash'], function(module, LoggerFactory, ngModule, _) {
    'use strict';
 
-   var logger = LoggerFactory.getInstance(module.id);
-
    return {
       register : function(moduleName, serviceArray) {
          if (!(serviceArray instanceof Array)) {
             throw new Error('Service function must be provided in array form');
          }
-         moduleName = moduleName.replace(/\//g, '.');
-         var serviceName = _.last(moduleName.split('.'));
-         logger.debug('Creating service [' + moduleName + ']');
-
-         var serviceFn = serviceArray.pop();
 
          var serviceLogger = LoggerFactory.getInstance(moduleName);
+         var serviceName = _.last(moduleName.split('\/'));
+         serviceLogger.debug('Creating service [' + moduleName + ']');
+
+         var serviceFn = serviceArray.pop();
 
          ngModule.service(serviceName, serviceArray.concat(function() {
             this.logger = serviceLogger;
             serviceFn.apply(this, arguments);
          }));
-         logger.debug('Service [' + moduleName + '] created');
+         serviceLogger.debug('Service [' + moduleName + '] created');
       }
    };
 });
