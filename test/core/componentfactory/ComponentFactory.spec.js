39fafad09b1ab416584380ad4dcb9aa6cb8c0148
Andrey Shustariov <shustarev.andrey@gmail.com>
2014-04-27 16:27:28 +0300

    Added more tests to ComponentFactory

--- test/core/componentfactory/ComponentFactory.spec.js
+++ test/core/componentfactory/ComponentFactory.spec.js
@@ -8,23 +8,44 @@ define([
 
    describe(module.id, function() {
 
-      var testComponent = {
+      var badComponent = {
          template : '<span></span>',
-         controller: function($scope) {
-            $scope.test = 'test';
+         controller: function() {
          }
       };
 
-      function registerTestComponent() {
-         ComponentFactory.register('test', testComponent);
+      var component = {
+         template : '<span></span>',
+         controller : ['$scope', function($scope) {
+            $scope.test = 'test';
+            controllerContext = this;
+         }]
+      };
+
+      var controllerContext;
+
+      function registerTestComponent(name, component) {
+         ComponentFactory.register(name, component);
       }
 
 
       it('should fail, when controller is not in array format', function() {
          function f () {
-            registerTestComponent();
+            registerTestComponent('badComponent', badComponent);
          }
          expect(f).toThrow();
+      });
+
+      it('should inject logger and lodash into controller context', function() {
+         registerTestComponent('test', component);
+         var $injector = angular.injector(['ngModule']);
+         var $rootScope = $injector.get('$rootScope');
+         var $compile = $injector.get('$compile');
+         var $scope = $rootScope.$new();
+         var element = angular.element('<test></test>');
+         $compile(element)($scope);
+         expect(controllerContext.logger).toBeDefined();
+         expect(controllerContext._).toBeDefined();
       });
    });
 });
